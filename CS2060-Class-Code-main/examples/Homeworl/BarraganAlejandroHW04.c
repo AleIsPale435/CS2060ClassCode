dbf4ca3669487b3ea8a58034fc3f5300ec808cda
AleIsPale435 <alebarr564@gmail.com>
2022-09-28 16:06:29 -0600

    Create BarraganAlejandroHW04.c

---
+++ CS2060-Class-Code-main/examples/Homeworl/BarraganAlejandroHW04.c
@@ -0,0 +1,19 @@
+/*
+Alejandro Barragan
+Assignment 3
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define STUDENTS 4
+#define GRADE_CATEGORIES 5
+#define CATEGORIES "1. Learning Activity 2. Homework 3. Project 4. Midterm 5. Final "
+#define MAX_GRADE 105
+#define MIN_GRADE 0
+const double GRADE_CATEGORY_WEIGHT[] = { 0.1, 0.3, 0.3, 0.15, .15 };
+
+
+int main(void) {
+
+}
