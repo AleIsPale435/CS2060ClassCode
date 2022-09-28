bb266ce28228da8cadaa568d6e534942f205ed49
AleIsPale435 <alebarr564@gmail.com>
2022-09-28 14:20:26 -0600

    Update ch07pointerExploration1.c

--- CS2060-Class-Code-main/examples/ch07/ch07pointerExploration1.c
+++ CS2060-Class-Code-main/examples/ch07/ch07pointerExploration1.c
@@ -1,7 +1,19 @@
 /*******************
  Understanding pointers
-
- Add comments to explain the code
+ - Using the asterisk and and symbol throughout 
+ to print out the address but not only the 
+ address but it also points to the variable that 
+ is stored in the address itself
+ - New format specifier %p is used to print out 
+ an address 
+ - The pointer itself has a different address 
+ than the pointer is pointing to
+ - If the pointer is pointing to another address, 
+ it will be the same because it is referring to 
+ that other address
+ - Using both the & and asterisk can show the 
+ same address because it is pointing to the 
+ address of the pointer it is pointing to
 **************/
 
 #include <stdio.h>
