29af0ff25412c4f8c7e265e7657e18ba51c1c464
AleIsPale435 <alebarr564@gmail.com>
2022-09-29 13:30:38 -0600

    Update BarraganAlejandroHW04.c

--- CS2060-Class-Code-main/examples/Homeworl/BarraganAlejandroHW04.c
+++ CS2060-Class-Code-main/examples/Homeworl/BarraganAlejandroHW04.c
@@ -1,6 +1,6 @@
 /*
 Alejandro Barragan
-Assignment 3
+Homework 04
 */
 #include <stdio.h>
 #include <stdlib.h>
@@ -13,7 +13,42 @@ Assignment 3
 #define MIN_GRADE 0
 const double GRADE_CATEGORY_WEIGHT[] = { 0.1, 0.3, 0.3, 0.15, .15 };
 
+double getInput(double value);
+
 
 int main(void) {
 
+	double studentInput = 0;
+	double grades[STUDENTS][GRADE_CATEGORIES];
+	bool validScan = true;
+	
+	do {
+		studentInput = getInput(studentInput);
+
+		if (studentInput > MAX_GRADE || studentInput < MIN_GRADE) {
+			validScan = false;
+		}
+
+	}
+
+
+}
+
+double getInput(double value) {
+
+	int validScan = 0;
+	double input = 0;
+
+	while (validScan <= 0) {
+
+		validScan = scanf("%f", &input);
+		while (getchar != '\n');
+
+		if (validScan <= 0) {
+			printf("%s", "Invalid try again");
+		}
+		else {
+			return value;
+		}
+	}
 }
