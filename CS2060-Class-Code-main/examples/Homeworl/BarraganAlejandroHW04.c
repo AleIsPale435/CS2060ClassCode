f13b1751bb291f30ee6dc174089f41ac8df10a83
AleIsPale435 <alebarr564@gmail.com>
2022-09-30 16:34:45 -0600

    Update BarraganAlejandroHW04.c

--- CS2060-Class-Code-main/examples/Homeworl/BarraganAlejandroHW04.c
+++ CS2060-Class-Code-main/examples/Homeworl/BarraganAlejandroHW04.c
@@ -12,43 +12,95 @@ Homework 04
 #define MAX_GRADE 105
 #define MIN_GRADE 0
 const double GRADE_CATEGORY_WEIGHT[] = { 0.1, 0.3, 0.3, 0.15, .15 };
+const double GRADE_A = 90;
+const double GRADE_B = 80;
+const double GRADE_C = 70;
+const double GRADE_D = 60;
 
-double getInput(double value);
+double getInput();
+double calculateGrade(double grades[][GRADE_CATEGORIES], size_t students, size_t gradeCats);
+void printTotalGrade(double finalGrades[]);
 
 
 int main(void) {
 
-	double studentInput = 0;
 	double grades[STUDENTS][GRADE_CATEGORIES];
-	bool validScan = true;
-	
-	do {
-		studentInput = getInput(studentInput);
+	calculateGrade(grades, STUDENTS, GRADE_CATEGORIES);
 
-		if (studentInput > MAX_GRADE || studentInput < MIN_GRADE) {
-			validScan = false;
+	double finalGrades[STUDENTS];
+	
+	for (size_t i = 0; i < STUDENTS; i++) {
+		double totalSum = 0;
+		for (size_t j = 0; j < GRADE_CATEGORIES; j++) {
+			totalSum += grades[i][j] * GRADE_CATEGORY_WEIGHT[j];
+
 		}
+		finalGrades[i] = totalSum;
 
 	}
+	
+	printTotalGrade(finalGrades);
 
 
 }
 
-double getInput(double value) {
+double getInput() {
 
 	int validScan = 0;
 	double input = 0;
+	bool valid = true;
 
-	while (validScan <= 0) {
-
-		validScan = scanf("%f", &input);
-		while (getchar != '\n');
+	do {
+		validScan = scanf("%lf", &input);
+		while (getchar() != '\n');
 
 		if (validScan <= 0) {
-			printf("%s", "Invalid try again");
+			puts("Invalid try again");
 		}
 		else {
-			return value;
+			if (input < MIN_GRADE || input > MAX_GRADE) {
+				puts("Invalid try again");
+			}
+			else {
+				return input;
+			}
 		}
+
+	}
+
+	while (valid);
+
+}
+
+double calculateGrade(double grades[][GRADE_CATEGORIES], size_t students, size_t gradeCats) {
+
+	for (size_t i = 0; i < STUDENTS; i++) {
+			for (size_t j = 0; j < GRADE_CATEGORIES; j++) {
+				printf("Enter grade for student %d in catagory %d\n", ((int)i + 1), ((int)j + 1));
+				grades[i][j] = getInput();
+			}
+		}
+}
+
+void printTotalGrade(double finalGrades[]) {
+
+	char letterGrade = ' ';
+	for (size_t i = 0; i < STUDENTS; i++) {
+		if (finalGrades[i] < MAX_GRADE && finalGrades[i] > 90) {
+			letterGrade = 'A';
+		}
+		else if (finalGrades[i] < 89 && finalGrades[i] > 80) {
+			letterGrade = 'B';
+		}
+		else if (finalGrades[i] < 79 && finalGrades[i] > 70) {
+			letterGrade = 'C';
+		}
+		else if (finalGrades[i] < 69 && finalGrades[i] > 60){
+			letterGrade = 'D';
+		}
+		else {
+			letterGrade = 'F';
+		}
+		printf("Student &d Grade: %.1lf %c", i, finalGrades[i], letterGrade);
 	}
 }
